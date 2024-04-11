da413dfc4e56fe5b1bb6617167b427dc7a846bcb
Vardui Saribekian <vartvioline@gmail.com>
2024-04-11 19:33:35 +0200

    Update

--- spolecne.js
+++ spolecne.js
@@ -3,12 +3,17 @@ const menu = document.querySelector('#menu-polozky');
 
 let menuVisible = false;
 
-tlacitko.addEventListener('click', () => {
-  if (!menuVisible) {
-    menu.classList.add('show');
-    menuVisible = true;
-  } else {
-    menu.classList.remove('show');
+const klikniMenu = tlacitko.addEventListener('click', () => {
+  let icon = document.querySelector('#menu-tlacitko i');
+  if (menuVisible) {
     menuVisible = false;
+    menu.classList.add('show');
+    icon.classList.add('fa-xmark');
+    icon.classList.remove('fa-bars');
+  } else {
+    menuVisible = true;
+    menu.classList.remove('show');
+    icon.classList.add('fa-bars');
+    icon.classList.remove('fa-xmark');
   }
 });
